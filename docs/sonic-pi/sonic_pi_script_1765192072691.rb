364aa7e109fbc364d8cb9920ff41d0d8857e05f9
Steven Doolan <steven.doolan@flightcentre.com.au>
2026-04-21 08:16:09 +1000

    Add new Sonic Pi scripts and move older ones to a new folder

--- docs/sonic-pi/sonic_pi_script_1765192072691.rb
+++
@@ -1,9 +0,0 @@
-# Mozartian Prelude
-use_synth :blade
-use_bpm 120
-
-# Melody inspired by Mozart
-notes = [:c4, :e4, :g4, :b4, :c5, :b4, :g4, :e4, :c4]
-durations = [0.5, 0.5, 0.5, 0.5, 1, 1, 0.5, 0.5, 2]
-
-play_pattern_timed notes, durations
